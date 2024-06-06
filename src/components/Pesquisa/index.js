2950ee19bd9ad217c6ffd6d96d51a7223e81f75e
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-06-05 21:16:17 -0300

    05-06

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,6 +1,7 @@
 
 import Input from "../Input";
 import styled from "styled-components"
+import { useState } from "react";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -23,11 +24,16 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa () {
+    const [ textoDigitado, setTextoDigitado] = useState('')
+
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
-            <Input placeholder="Escreva sua próxima leitura" />
+            <Input placeholder="Escreva sua próxima leitura" 
+            onBlur={evento => setTextoDigitado(evento.target.value)}            
+           />
+           <p>{textoDigitado}</p>
         </PesquisaContainer>
     )
 }
